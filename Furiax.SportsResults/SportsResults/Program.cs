15522314735f2e033bf9050f950ea22169a2e061
furiax <carlmalfliet@proximus.be>
2023-08-27 21:45:36 +0200

    added and tested the mail function

---
+++ Furiax.SportsResults/SportsResults/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+SendMail();
+
+static void SendMail()
+{
+	string smtpAddress = "smtp.gmail.com";
+	int portNumber = 587;
+	bool enableSSL = true;
+	string emailFromAddress = "furiaxtest@gmail.com";
+	string password = "cpgzpekzbeapbllg";
+	string emailToAddress = "carlmalfliet@proximus.be";
+	string subject = "Sport results";
+	string body = "Hello, this is a test email. Soon this will contain the sport results";
+	using (MailMessage mail = new MailMessage())
+	{
+		mail.From = new MailAddress(emailFromAddress);
+		mail.To.Add(emailToAddress);
+		mail.Subject = subject;
+		mail.Body = body;
+		mail.IsBodyHtml = true;
+		using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+		{
+			smtp.Credentials = new NetworkCredential(emailFromAddress, password);
+			smtp.EnableSsl = enableSSL;
+			smtp.Send(mail);
+		}
+	}
+}
