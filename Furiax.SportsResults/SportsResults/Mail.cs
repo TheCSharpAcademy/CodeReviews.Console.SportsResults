ca340a08c2d1286be86f9551e50e69c151cc8944
furiax <carlmalfliet@proximus.be>
2023-08-29 20:27:04 +0200

    refactoring

---
+++ Furiax.SportsResults/SportsResults/Mail.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace SportsResults
+{
+	internal class Mail
+	{
+		public static void SendMail()
+		{
+			string smtpAddress = "smtp.gmail.com";
+			int portNumber = 587;
+			bool enableSSL = true;
+			string emailFromAddress = "furiaxtest@gmail.com";
+			string password = "";
+			string emailToAddress = "carlmalfliet@proximus.be";
+			string subject = "Sport results";
+			string body = "Hello, this is a test email. Soon this will contain the sport results";
+			using (MailMessage mail = new MailMessage())
+			{
+				mail.From = new MailAddress(emailFromAddress);
+				mail.To.Add(emailToAddress);
+				mail.Subject = subject;
+				mail.Body = body;
+				mail.IsBodyHtml = true;
+				try
+				{
+					using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+					{
+						smtp.Credentials = new NetworkCredential(emailFromAddress, password);
+						smtp.EnableSsl = enableSSL;
+						smtp.Send(mail);
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Error mail not send: {ex.Message}");
+				}
+
+			}
+		}
+	}
+}
--- Furiax.SportsResults/SportsResults/Program.cs
+++ Furiax.SportsResults/SportsResults/Program.cs
@@ -1,30 +1,4 @@
-﻿using System.Net;
-using System.Net.Mail;
+﻿using SportsResults;
 
-SendMail();
+Mail.SendMail();
 
-static void SendMail()
-{
-	string smtpAddress = "smtp.gmail.com";
-	int portNumber = 587;
-	bool enableSSL = true;
-	string emailFromAddress = "furiaxtest@gmail.com";
-	string password = "****";
-	string emailToAddress = "carlmalfliet@proximus.be";
-	string subject = "Sport results";
-	string body = "Hello, this is a test email. Soon this will contain the sport results";
-	using (MailMessage mail = new MailMessage())
-	{
-		mail.From = new MailAddress(emailFromAddress);
-		mail.To.Add(emailToAddress);
-		mail.Subject = subject;
-		mail.Body = body;
-		mail.IsBodyHtml = true;
-		using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
-		{
-			smtp.Credentials = new NetworkCredential(emailFromAddress, password);
-			smtp.EnableSsl = enableSSL;
-			smtp.Send(mail);
-		}
-	}
-}
