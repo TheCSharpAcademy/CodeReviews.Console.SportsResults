761771e5c2e3c121827ecc7e195901b29f1fa26c
Michiel <michiel.vergauwen@visma.com>
2023-12-08 12:52:35 +0100

    Finished string of winners

--- SportsResultsApp.MicVerg/SportsResults/Program.cs
+++ SportsResultsApp.MicVerg/SportsResults/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+var url = "https://www.basketball-reference.com/boxscores/";
+var web = new HtmlWeb();
+var loadedDocument = web.Load(url);
+
+var gameSummaryNodes = loadedDocument.DocumentNode.SelectNodes("//*[contains(@class, 'winner')]");
+
+Console.WriteLine("The winning teams today are: \n");
+
+foreach (var node in gameSummaryNodes)
+{
+    string innerText = node.InnerText.Trim();
+
+    // clean string
+    innerText = Regex.Replace(innerText, @"\s+", " "); // Replace multiple spaces with a single space
+    innerText = innerText.Replace("\n", "").Replace("\t", ""); // Remove newline and tab characters
+    innerText = innerText.Replace("&nbsp;", ""); // Remove non-breaking space
+    innerText = innerText.Replace("Final", "", StringComparison.OrdinalIgnoreCase);
+    string teamName = Regex.Match(innerText, @"^[^\d]+").Value.Trim();
+
+
+    Console.WriteLine(teamName);
+}
+
+Console.ReadLine();
