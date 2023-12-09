99a3a9cf92a322b3a4ca077a580caf22a09b092d
Michiel <michiel.vergauwen@visma.com>
2023-12-09 09:16:00 +0100

    Finished (?)

--- SportsResultsApp.MicVerg/SportsResults/Program.cs
+++ SportsResultsApp.MicVerg/SportsResults/Program.cs
@@ -1,28 +1,5 @@
-﻿
-using HtmlAgilityPack;
-using System.Text.RegularExpressions;
+﻿using SportsResults;
 
-var url = "https://www.basketball-reference.com/boxscores/";
-var web = new HtmlWeb();
-var loadedDocument = web.Load(url);
+string mailBody = string.Join("\n", GetWinningTeams.WinningNbaTeams());
+SendMail.SendEmail(mailBody);
 
-var gameSummaryNodes = loadedDocument.DocumentNode.SelectNodes("//*[contains(@class, 'winner')]");
-
-Console.WriteLine("The winning teams today are: \n");
-
-foreach (var node in gameSummaryNodes)
-{
-    string innerText = node.InnerText.Trim();
-
-    // clean string
-    innerText = Regex.Replace(innerText, @"\s+", " "); // Replace multiple spaces with a single space
-    innerText = innerText.Replace("\n", "").Replace("\t", ""); // Remove newline and tab characters
-    innerText = innerText.Replace("&nbsp;", ""); // Remove non-breaking space
-    innerText = innerText.Replace("Final", "", StringComparison.OrdinalIgnoreCase);
-    string teamName = Regex.Match(innerText, @"^[^\d]+").Value.Trim();
-
-
-    Console.WriteLine(teamName);
-}
-
-Console.ReadLine();
