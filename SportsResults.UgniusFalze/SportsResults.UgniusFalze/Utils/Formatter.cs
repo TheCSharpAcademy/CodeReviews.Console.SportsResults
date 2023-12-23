7183b30c237a47991b9d5cf7c56398e579cf3ca9
ugnius <ugnius.vilkoncius@gmail.com>
2023-12-23 21:11:33 +0200

    Cleaned up code

--- SportsResults.UgniusFalze/SportsResults.UgniusFalze/Utils/Formatter.cs
+++ SportsResults.UgniusFalze/SportsResults.UgniusFalze/Utils/Formatter.cs
@@ -10,40 +10,40 @@ public static class Formatter
         var gamesString = new StringBuilder()
             .AppendLine("<html>")
             .AppendLine("<body>");
-        if (games.Count == 0)
+        if (games.Count != 0)
         {
             gamesString
-                .AppendLine("<p>No games were played today</p>")
-                .AppendLine("</body>")
-                .AppendLine("</html>");
-            return gamesString.ToString();
-        }
-
-        gamesString
-            .AppendLine("<table>")
-            .AppendLine("<tr>")
-            .AppendLine("<th>Game number</th>")
-            .AppendLine("<th>Winner</th>")
-            .AppendLine("<th>Winner Score</th>")
-            .AppendLine("<th>Looser</th>")
-            .AppendLine("<th>Looser Score</th>")
-            .AppendLine("</tr>");
-        for (var i = 0; i < games.Count; i++)
-        {
-            var game = games[i];
-            gamesString
+                .AppendLine("<table>")
                 .AppendLine("<tr>")
-                .AppendLine($"<td>{i + 1}</td>")
-                .AppendLine($"<td>{game.Winner}</td>")
-                .AppendLine($"<td>{game.WinnerScore}</td>")
-                .AppendLine($"<td>{game.Looser}</td>")
-                .AppendLine($"<td>{game.LooserScore}</td>")
+                .AppendLine("<th>Game number</th>")
+                .AppendLine("<th>Winner</th>")
+                .AppendLine("<th>Winner Score</th>")
+                .AppendLine("<th>Looser</th>")
+                .AppendLine("<th>Looser Score</th>")
                 .AppendLine("</tr>");
+            for (var i = 0; i < games.Count; i++)
+            {
+                var game = games[i];
+                gamesString
+                    .AppendLine("<tr>")
+                    .AppendLine($"<td>{i + 1}</td>")
+                    .AppendLine($"<td>{game.Winner}</td>")
+                    .AppendLine($"<td>{game.WinnerScore}</td>")
+                    .AppendLine($"<td>{game.Looser}</td>")
+                    .AppendLine($"<td>{game.LooserScore}</td>")
+                    .AppendLine("</tr>");
+            }
+            
+        }
+        else
+        {
+            gamesString
+                .AppendLine("<p>No games were played today</p>");
         }
 
         gamesString
             .AppendLine("</body>")
-            .AppendLine("</html>");
+            .AppendLine("</html>");        
         return gamesString.ToString();
     }
 }
