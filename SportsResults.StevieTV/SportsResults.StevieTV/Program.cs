14c0bec4387628549102fad4c40ecf306527ff5d
Stevie Robinson <stevie.robinson@gmail.com>
2024-01-24 21:59:13 +0100

    SportsResults project complete

---
+++ SportsResults.StevieTV/SportsResults.StevieTV/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SportsResults.StevieTV.EmailService;
+using SportsResults.StevieTV.Scraper;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+builder.Services.AddSingleton<IGameScraper, GameScraper>();
+builder.Services.AddHostedService<EmailService>();
+
+
+using IHost host = builder.Build();
+
+await host.RunAsync();
