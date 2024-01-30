b59dbc22ae0ef420b211d5e29207ef17c594fd67
doc415 <sipahioglu_serdar@yahoo.com>
2024-01-31 01:26:44 +0300

    completed notifier

---
+++ SportsNotifier.Doc415/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SportsNotifier;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var host = Host.CreateDefaultBuilder(args).ConfigureServices((hostContext, services) =>
+        {
+            services.AddHostedService<ScoreResultsService>();
+            services.AddSingleton<WebScrape>();
+            services.AddSingleton<EmailSender>();
+            services.AddSingleton<MessageBuilder>();
+        }).Build();
+        host.Run();
+    }
+}
