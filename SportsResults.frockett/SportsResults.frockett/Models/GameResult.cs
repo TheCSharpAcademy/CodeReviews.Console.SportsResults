ae8019777ec12e1a9e11b73149b8fe1b5f5f9256
frockett <crockett.d.ford@gmail.com>
2024-03-21 11:12:17 +0800

    initial commit

---
+++ SportsResults.frockett/SportsResults.frockett/Models/GameResult.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace SportsResults.frockett.Models;
+
+internal class GameResult
+{
+    public string HomeTeam { get; set; }
+    public string AwayTeam { get; set; }
+    public string HomeFinalScore { get; set; }
+    public string AwayFinalScore { get; set; }
+}
