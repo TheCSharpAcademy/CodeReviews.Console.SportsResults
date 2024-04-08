8fb69876dc8449e241dafd025f1b96f372a1dd44
Dejmenek <dejmen14567@gmail.com>
2024-04-08 16:41:59 +0200

    Add dependency injection

---
+++ SportsResults.Dejmenek/Program.cs
@@ -0,0 +1,29 @@
+﻿using HtmlAgilityPack;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SportsResults.Dejmenek.Services;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var services = new ServiceCollection();
+
+services.AddSingleton(configuration);
+services.AddSingleton<HtmlWeb>();
+services.AddSingleton<INbaDataProcessorService, NbaDataProcessorService>();
+services.AddSingleton<INbaDataScraperService, NbaDataScraperService>();
+services.AddSingleton<IEmailNotificationService, EmailNotificationService>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var scraper = serviceProvider.GetRequiredService<INbaDataScraperService>();
+var processor = serviceProvider.GetRequiredService<INbaDataProcessorService>();
+var emailService = serviceProvider.GetRequiredService<IEmailNotificationService>();
+
+var easternStandings = scraper.ScrapeEasternConferenceStandings();
+var westernStandings = scraper.ScrapeWesternConferenceStandings();
+var games = scraper.ScrapeGames();
+string emailBody = processor.PrepareEmailBody(games, easternStandings, westernStandings);
+emailService.SendEmail(emailBody);
