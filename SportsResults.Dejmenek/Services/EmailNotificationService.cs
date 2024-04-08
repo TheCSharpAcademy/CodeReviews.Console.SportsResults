592033644a050b4e2b8eb216fb22a630f3b440d2
Dejmenek <dejmen14567@gmail.com>
2024-04-08 16:41:48 +0200

    Add services

---
+++ SportsResults.Dejmenek/Services/EmailNotificationService.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.Mail;
+
+namespace SportsResults.Dejmenek.Services;
+public class EmailNotificationService : IEmailNotificationService
+{
+    private readonly SmtpClient _smtpClient;
+    private readonly MailMessage _mailMessage;
+
+    public EmailNotificationService(IConfiguration configuration)
+    {
+        _smtpClient = new SmtpClient(
+            configuration["SmtpConfiguration:Server"],
+            int.Parse(configuration["SmtpConfiguration:Port"])
+        );
+        _smtpClient.EnableSsl = true;
+        _smtpClient.UseDefaultCredentials = false;
+        _smtpClient.Credentials = new NetworkCredential(
+            configuration["SmtpConfiguration:SenderUsername"],
+            configuration["SmtpConfiguration:SenderPassword"]
+        );
+        _mailMessage = new MailMessage(
+            configuration["SmtpConfiguration:SenderUsername"],
+            configuration["SmtpConfiguration:ReceiverEmail"]
+        );
+        _mailMessage.Subject = "Latest NBA Updates";
+        _mailMessage.IsBodyHtml = true;
+    }
+
+    public void SendEmail(string body)
+    {
+        _mailMessage.Body = body;
+
+        try
+        {
+            _smtpClient.Send(_mailMessage);
+            Console.WriteLine("Email sent");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Failed to send email: {ex.Message}");
+        }
+    }
+}
---
+++ SportsResults.Dejmenek/Services/NbaDataProcessorService.cs
@@ -0,0 +1,80 @@
+﻿using HandlebarsDotNet;
+using SportsResults.Dejmenek.Models;
+
+namespace SportsResults.Dejmenek.Services;
+public class NbaDataProcessorService : INbaDataProcessorService
+{
+    private static string mailTemplate = """
+        <h1>Latest NBA Updates - {{ Date }}</h1>
+        <h2>Today's Games</h2>
+        {{#if Games}}
+            <div>
+            {{#each Games}}
+            <div style="border: 1px solid black; margin-bottom: 10px">
+                <div><p>{{ HomeTeam.Name }} - {{HomeTeamScore}}</p></div>
+                <div><p>{{ AwayTeam.Name }} - {{AwayTeamScore}}</p></div>
+            </div>
+            {{/each}}
+            </div>
+        {{else}}
+            <p>No games played yet</p>
+        {{/if}}
+        <h2>Eastern Conference Standings</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Team</th>
+              <th>Wins</th>
+              <th>Losses</th>
+              <th>%</th>
+            </tr>
+          </thead>
+          <tbody>
+            {{#each EasternStandings}}
+            <tr>
+              <td>{{ Team.Name }}</td>
+              <td>{{ Wins }}</td>
+              <td>{{ Losses }}</td>
+              <td>{{ WinPercentage }}</td>
+            </tr>
+            {{/each}}
+          </tbody>
+        </table>
+        <h2>Western Conference Standings</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Team</th>
+              <th>Wins</th>
+              <th>Losses</th>
+              <th>%</th>
+            </tr>
+          </thead>
+          <tbody>
+            {{#each WesternStandings}}
+            <tr>
+              <td>{{ Team.Name }}</td>
+              <td>{{ Wins }}</td>
+              <td>{{ Losses }}</td>
+              <td>{{ WinPercentage }}</td>
+            </tr>
+            {{/each}}
+          </tbody>
+        </table>
+        """;
+
+    public string PrepareEmailBody(List<Game> games, List<TeamStanding> easternConferenceStandings, List<TeamStanding> westernConferenceStandings)
+    {
+        var data = new
+        {
+            Date = DateTime.Now.ToString("dd/MM/yyyy"),
+            Games = games,
+            EasternStandings = easternConferenceStandings,
+            WesternStandings = westernConferenceStandings
+        };
+
+        var body = Handlebars.Compile(mailTemplate)(data);
+
+        return body;
+    }
+}
