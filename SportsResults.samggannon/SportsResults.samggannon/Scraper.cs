c01ce4ea444cf136451c82a145e3b359509c72a8
Samuel <Sam.Gannon@sportssouth.biz>
2024-05-21 13:06:40 -0500

    / example scrape

---
+++ SportsResults.samggannon/SportsResults.samggannon/Scraper.cs
@@ -0,0 +1,19 @@
+﻿using HtmlAgilityPack;
+
+namespace SportsResults.samggannon;
+
+public class Scraper
+{
+    public static void Scrape()
+    {
+        HtmlWeb web = new HtmlWeb();
+        HtmlDocument document = web.Load("https://example.com");
+
+        var title = document.DocumentNode.SelectNodes("//div/h1").First().InnerHtml;
+        var description = document.DocumentNode.SelectNodes("//div/p").First().InnerHtml;
+
+        Console.WriteLine(title);
+        Console.WriteLine(description);
+        Console.ReadLine();
+    }
+}
