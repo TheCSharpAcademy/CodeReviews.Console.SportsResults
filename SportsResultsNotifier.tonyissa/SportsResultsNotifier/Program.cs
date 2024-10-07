3bc00666cf423a7bcb71f438e4f89ecb4ad37d41
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-10-07 14:09:56 -0400

    Implement scraping service logic and add to DI + logging

--- SportsResultsNotifier.tonyissa/SportsResultsNotifier/Program.cs
+++ SportsResultsNotifier.tonyissa/SportsResultsNotifier/Program.cs
@@ -1,8 +1,16 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using SportsResultsNotifier.Services;
+using Microsoft.Extensions.Logging;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-
+builder.Services.AddTransient<ScrapingService>();
+builder.Services.AddLogging(logger =>
+{
+    logger.ClearProviders();
+    logger.AddConsole();
+});
 
 var app = builder.Build();
 
---
+++ SportsResultsNotifier.tonyissa/SportsResultsNotifier/Services/ScrapingService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SportsResultsNotifier.Services;
+
+public class ScrapingService : BackgroundService
+{
+    private readonly ILogger<ScrapingService> _logger;
+    private readonly TimeSpan _interval = TimeSpan.FromHours(24);
+
+    public ScrapingService(ILogger<ScrapingService> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Scraping service is starting.");
+
+        while (stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                _logger.LogInformation("Daily task started at: {time}", DateTimeOffset.Now);
+
+                await DoSomething(stoppingToken);
+
+                _logger.LogInformation("Daily task completed at: {time}", DateTimeOffset.Now);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error has occured");
+            }
+
+            await Task.Delay(_interval, stoppingToken);
+        }
+
+        _logger.LogInformation("Scraping service is stopping.");
+    }
+
+    private async Task DoSomething(CancellationToken stoppingToken)
+    {
+
+    }
+}
