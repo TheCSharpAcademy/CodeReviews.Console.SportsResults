b220aefb387ae50f111566df8dbb4a3638bd8702
Arashi256 <arashi256@hotmail.com>
2024-10-25 20:20:37 +0100

    SportsResultsNotifier project completed

---
+++ SportsResultsNotifier.Arashi256/Models/Contact.cs
@@ -0,0 +1,9 @@
+﻿
+namespace SportsResultsNotifier.Arashi256.Models
+{
+    internal class Contact
+    {
+        public string? Name { get; set; } = string.Empty;
+        public string? Address { get; set; } = string.Empty;
+    }
+}
