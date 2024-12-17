a98de7934c7d8cdb2c739d0bf71c9a0ddb0435f8
jollejonas <jollevb@hotmail.com>
2024-12-17 22:33:30 +0100

    - Implemented a daily operation, to send the sports results.

--- SportsResults.jollejonas/SportsResults.jollejonas/Program.cs
+++ SportsResults.jollejonas/SportsResults.jollejonas/Program.cs
@@ -4,20 +4,43 @@ using SportsResults.jollejonas.Services;
 
 public class Program
 {
+    private static Timer? _timer;
     public static void Main()
     {
         var configuration = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
             .Build();
+
+        Console.WriteLine("Application has started. Scheduling daily task...");
+
+        ScheduleDailyTask(configuration);
+        Console.ReadLine();
+    }
+    private static void ScheduleDailyTask(IConfiguration configuration)
+    {
+        TimeSpan runTime = new TimeSpan(8, 0, 0);
+        TimeSpan currentTime = DateTime.Now.TimeOfDay;
+        TimeSpan firstRun = (runTime > currentTime) ? runTime - currentTime : runTime - currentTime + new TimeSpan(24, 0, 0);
+
+        _timer = new Timer(_ =>
+        {
+            ExecuteTask(configuration);
+        }, null, firstRun, new TimeSpan(0, 5, 0));
+    }
+
+    private static void ExecuteTask(IConfiguration configuration)
+    {
         Mail mail = new Mail(configuration);
         Scraper scraper = new Scraper();
         string message = scraper.Scrape();
 
-        if (message == "")
+        if (string.IsNullOrEmpty(message))
         {
-            Console.WriteLine("Error while scraping");
+            Console.WriteLine($"{DateTime.Now}: Error while scraping");
             return;
         }
+
         mail.SendMail(message);
+        Console.WriteLine($"{DateTime.Now}: Email sent successfully.");
     }
 }
