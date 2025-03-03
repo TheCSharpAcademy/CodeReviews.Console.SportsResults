1c9f2daa3141e5962e6d76d00bc2639f78c7f797
anaclos <anaclos2@yahoo.com.ar>
2025-03-03 17:37:41 -0300

    New finished

---
+++ SportsResults.AnaClos/SportsResults/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SportsResults;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddHostedService<SportsController>();
+builder.Services.AddSingleton<EmailService>();
+builder.Services.AddSingleton<ScrappingService>();
+builder.Services.AddSingleton<ConfigurationService>();
+
+var host = builder.Build();
+host.Run();
+
+
