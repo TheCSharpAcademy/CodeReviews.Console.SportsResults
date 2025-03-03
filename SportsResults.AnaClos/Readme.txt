43c1df923d66a0bfe183e7511548912af03a64fd
anaclos <anaclos2@yahoo.com.ar>
2025-03-03 16:05:22 -0300

    Finished #1

---
+++ SportsResults.AnaClos/Readme.txt
@@ -0,0 +1,11 @@
+# Console Sports Results
+## Description
+In this application, sports data is read from a website once a day and sent to a specific email address.
+## Google 
+I had to create an app password in order to send emails from my Google account.
+## Configuration
+Mail settings are read from the appsettings.json file.
+## Time Interval
+If you want to try another time interval, go to SportsController > ExecuteAsync and modify the time span of the timer. TimeSpan.FromDays(1)
+## Improvements
+Create my own scrapper from a different source and with a different business case.
---
+++ SportsResults.AnaClos/SportsResults/ScrappingService.cs
@@ -0,0 +1,49 @@
+﻿using HtmlAgilityPack;
+
+namespace SportsResults;
+
+public class ScrappingService
+{
+    string url = "https://www.basketball-reference.com/boxscores/";
+    HtmlWeb htmlWeb = new HtmlWeb();
+    HtmlDocument document;
+    public ScrappingService()
+    {
+        document = htmlWeb.Load(url);
+    }
+
+    public string GetTitle()
+    {
+        string subject = document.DocumentNode.SelectNodes("//div/h1").First().InnerText;
+        return subject;
+    }
+
+    public string GetResults()
+    {
+        string result = "";
+        int count = 1;
+        var gameSummariesDiv = document.DocumentNode.SelectSingleNode("//div[@class='game_summaries']");
+        var gameSummaryDivs = gameSummariesDiv.SelectNodes(".//div[contains(@class, 'game_summary expanded nohover')]");
+
+        foreach (var gameSummaryDiv in gameSummaryDivs)
+        {
+            var teamsTable = gameSummaryDiv.SelectSingleNode(".//table[@class='teams']");
+
+            var winnerRow = teamsTable.SelectSingleNode(".//tr[@class='winner']");
+
+            var winnerName = winnerRow.SelectSingleNode(".//a").InnerText;
+
+            var scoreWinner = winnerRow.SelectSingleNode(".//td[@class='right']").InnerHtml;
+
+            var loserRow = teamsTable.SelectSingleNode(".//tr[@class='loser']");
+
+            var loserName = loserRow.SelectSingleNode(".//a").InnerText;
+
+            var scoreLoser = loserRow.SelectSingleNode(".//td[@class='right']").InnerHtml;
+
+            result += $"Game #{count}{System.Environment.NewLine}{winnerName}: {scoreWinner}{System.Environment.NewLine}{loserName} {scoreLoser}{System.Environment.NewLine}{System.Environment.NewLine}";
+            count++;
+        }
+        return result;
+    }
+}
