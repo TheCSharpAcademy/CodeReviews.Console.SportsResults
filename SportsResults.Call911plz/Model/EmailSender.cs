c528a7a2f55e46f5d0293dc4f6b622a1d546b67c
Hieu Truong <htruong6219@gmail.com>
2025-04-26 15:36:20 -0700

    feat: add email sender

---
+++ SportsResults.Call911plz/Model/EmailSender.cs
@@ -0,0 +1,28 @@
+using System.Net;
+using System.Net.Mail;
+
+public class EmailSender(string email, string password)
+{
+    private readonly string _email = email;
+    private readonly string _password = password;
+
+    public void SendToSelf(string subject, string content)
+    {
+        SmtpClient smtp = new("smtp.gmail.com", 587)
+        {
+            Credentials = new NetworkCredential(_email, _password),
+            EnableSsl = true,
+        };
+        MailAddress from = new(_email);
+        MailAddress to = new(_email);
+
+        MailMessage message = new(from, to)
+        {
+            Subject = subject,
+            Body = content
+        };
+
+        try { smtp.Send(message); }
+        catch (Exception e) { Console.WriteLine(e); }
+    }
+}
--- SportsResults.Call911plz/Program.cs
+++ SportsResults.Call911plz/Program.cs
@@ -2,27 +2,33 @@
 
 class Program
 {
+    // Change these on use
+    private readonly static string EMAIL = "htruong6219@gmail.com";
+    private readonly static string SMTPGMAILPASSWORD = "";
     static void Main(string[] args)
     {
-        List<GameSummary> summaries = Scraper.GetSummaries(@"https://www.basketball-reference.com/boxscores/?month=04&day=19&year=2025");
-        foreach(var summary in summaries)
-        {
-            Console.WriteLine($"Winner: {summary.Winner.Name}");
-            Console.Write($"Score: ");
-            foreach (int score in summary.Winner.Scores)
-            {
-                Console.Write($"\t{score}");
-            }
-            Console.WriteLine();
+        EmailSender sender = new(EMAIL, SMTPGMAILPASSWORD);
+        sender.SendToSelf("testing", "worked");
+
+        // List<GameSummary> summaries = Scraper.GetSummaries(@"https://www.basketball-reference.com/boxscores/?month=04&day=19&year=2025");
+        // foreach(var summary in summaries)
+        // {
+        //     Console.WriteLine($"Winner: {summary.Winner.Name}");
+        //     Console.Write($"Score: ");
+        //     foreach (int score in summary.Winner.Scores)
+        //     {
+        //         Console.Write($"\t{score}");
+        //     }
+        //     Console.WriteLine();
 
 
-            Console.WriteLine($"Loser: {summary.Loser.Name}");
-            Console.Write($"Score: ");
-            foreach (int score in summary.Loser.Scores)
-            {
-                Console.Write($"\t{score}");
-            }
-            Console.WriteLine();
-        }
+        //     Console.WriteLine($"Loser: {summary.Loser.Name}");
+        //     Console.Write($"Score: ");
+        //     foreach (int score in summary.Loser.Scores)
+        //     {
+        //         Console.Write($"\t{score}");
+        //     }
+        //     Console.WriteLine();
+        // }
     }
 }
