b4bb87cc6365dc74cc48b1e7c54c1140219d87b8
Brozda <daniel.brozik@gmail.com>
2025-05-30 14:23:57 +0200

    feat: Add general project structure
    Added models and 2 services for the project
    
    Models:
     Team - Name, Score
     Stat - Name, Value
     Game - Team (winner), Team (looser), Stat (PTS), Stat (TRB)
    
    ScrapingService
    EmailService

---
+++ SportsResults.BrozDa/SportsResults.BrozDa/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SportsResults.BrozDa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
