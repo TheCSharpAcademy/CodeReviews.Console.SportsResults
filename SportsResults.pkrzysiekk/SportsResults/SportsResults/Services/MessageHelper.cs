109652605fd2fdc8679d93b85fc29b8ebf2164ee
pkrzysiek <pkrzysiek24@gmail.com>
2025-06-30 19:17:28 +0200

    Add MessageHelper to format body of email

---
+++ SportsResults.pkrzysiekk/SportsResults/SportsResults/Services/MessageHelper.cs
@@ -0,0 +1,33 @@
+using System.Text;
+using SportsResults.Models;
+
+namespace SportsResults;
+
+public static class MessageHelper
+{
+    private static bool AreNewsFromToday(IEnumerable<Match> matches)
+    {
+        return matches.First().Date==DateTime.Today;
+    }
+    public static string GetFormattedMessageBody(List<Match> matches)
+    {
+       StringBuilder sb = new StringBuilder();
+
+       sb.AppendLine(
+           $"Hello, here are the news for today ({DateTime.Now.Year}-{DateTime.Now.Month}-{DateTime.Now.Day}}})");
+       if (!AreNewsFromToday(matches))
+           sb.AppendLine("No matches from today found, the most recent matches:"); 
+       
+       sb.AppendLine($"Matches from  today: {matches.First().Date.ToShortDateString()}");
+       
+       foreach (var match in matches)
+       {    
+           sb.AppendLine(match.ToString());
+           sb.AppendLine("_________________________________________________________");
+       }
+
+       sb.AppendLine("Thank you for using our service, see you tomorrow!");
+       return sb.ToString();
+    }
+
+}
