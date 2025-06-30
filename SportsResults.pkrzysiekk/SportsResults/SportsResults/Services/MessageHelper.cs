bf3d684035d942b150f64312b5bdb50b2c09de94
pkrzysiek <pkrzysiek24@gmail.com>
2025-06-30 20:26:25 +0200

    Update MessageHelper

--- SportsResults.pkrzysiekk/SportsResults/SportsResults/Services/MessageHelper.cs
+++ SportsResults.pkrzysiekk/SportsResults/SportsResults/Services/MessageHelper.cs
@@ -12,18 +12,26 @@ public static class MessageHelper
     public static string GetFormattedMessageBody(List<Match> matches)
     {
        StringBuilder sb = new StringBuilder();
-
+       if (!matches.Any())
+       {
+           sb.AppendLine("No matches found for this season");
+           return sb.ToString();
+       }
        sb.AppendLine(
-           $"Hello, here are the news for today ({DateTime.Now.Year}-{DateTime.Now.Month}-{DateTime.Now.Day}}})");
+           $"Hello, here are the news for today ({DateTime.Now.Year}-{DateTime.Now.Month}-{DateTime.Now.Day})");
        if (!AreNewsFromToday(matches))
-           sb.AppendLine("No matches from today found, the most recent matches:"); 
-       
-       sb.AppendLine($"Matches from  today: {matches.First().Date.ToShortDateString()}");
+       {
+           sb.AppendLine("No matches from today found, the most recent matches:");
+           sb.AppendLine();
+       }
+
+       sb.AppendLine($"Matches from: {matches.First().Date.ToShortDateString()}");
        
        foreach (var match in matches)
        {    
            sb.AppendLine(match.ToString());
-           sb.AppendLine("_________________________________________________________");
+           sb.AppendLine("__________________________________");
+           sb.AppendLine();
        }
 
        sb.AppendLine("Thank you for using our service, see you tomorrow!");
