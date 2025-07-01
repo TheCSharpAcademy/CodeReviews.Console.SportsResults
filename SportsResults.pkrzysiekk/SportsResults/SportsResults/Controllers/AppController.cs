2023599913d8bb9a5e8804f008b1fb7acbe7c60b
pkrzysiek <pkrzysiek24@gmail.com>
2025-07-01 13:05:38 +0200

    Refactor the controller

--- SportsResults.pkrzysiekk/SportsResults/SportsResults/Controllers/AppController.cs
+++ SportsResults.pkrzysiekk/SportsResults/SportsResults/Controllers/AppController.cs
@@ -8,24 +8,26 @@ public class AppController
     public EmailSender _emailSender;
     private readonly Scrapper _scrapper;
     private Timer _timer;
+
     public AppController(string urlToScrap, EmailSender emailSender)
     {
         _emailSender = emailSender;
         _scrapper = new Scrapper(urlToScrap);
         _timer = new Timer(TimeSpan.FromHours(24).TotalMilliseconds);
-        _timer.Elapsed += Start;
+        _timer.Elapsed += Execute;
         _timer.AutoReset = true;
-        _timer.Start();
-        Start(null, null);
-        
-        
     }
 
-    public void Start(object sender, ElapsedEventArgs e)
+    public void StartController()
+    {
+        _timer.Start();
+        Execute(null, null);
+    }
+
+    private void Execute(object sender, ElapsedEventArgs e)
     {
         var matches = _scrapper.ScrapeMatches();
         var messageBody = MessageHelper.GetFormattedMessageBody(matches);
         _emailSender.Send(messageBody);
     }
-    
 }
--- SportsResults.pkrzysiekk/SportsResults/SportsResults/Program.cs
+++ SportsResults.pkrzysiekk/SportsResults/SportsResults/Program.cs
@@ -3,9 +3,9 @@
 using SportsResults;
 using SportsResults.Controllers;
 
- var mail= new EmailSender();
- mail.AddReceiver("pkrzysiek24@gmail.com");
-string url="https://www.basketball-reference.com/boxscores/";
-AppController ctr = new AppController(url,mail);
+var mail = new EmailSender();
+mail.AddReceiver("pkrzysiek24@gmail.com");
+string url = "https://www.basketball-reference.com/boxscores/";
+AppController ctr = new AppController(url, mail);
+ctr.StartController();
 Thread.Sleep(Timeout.Infinite);
-
