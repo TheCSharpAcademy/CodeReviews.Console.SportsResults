e012c1d8c26c7b9941fdd908a5177d59d98a6ba0
azeez-odetola <azeez.odetola@rhinoentertainmentgroup.com>
2025-07-20 15:04:47 +0100

    Project completed

--- SportResults.yemiodetola/Program.cs
+++ SportResults.yemiodetola/Program.cs
@@ -3,14 +3,29 @@ using SportResults.Models;
 using SportResults.Mailer;
 using SportResults.Helpers;
 
+var timer = new Timer(SendDailyEmail, null, 0, 86400000);
 
-string url = "https://www.basketball-reference.com/boxscores/";
+// Keep the app running
+Console.WriteLine("Press any key to stop...");
+Console.ReadKey();
 
-ScraperService scraperService = new ScraperService(url);
-List<Result> results = scraperService.ScrapGameData();
-
-string formattedResult = MailHelper.GenerateMailBody(results);
-
-Mailer.SendEmail(formattedResult);
-
-Console.WriteLine("Email sent.");
+static void SendDailyEmail(object? state)
+{
+    try
+    {
+        string url = "https://www.basketball-reference.com/boxscores/";
+        
+        ScraperService scraperService = new ScraperService(url);
+        List<Result> results = scraperService.ScrapGameData();
+        
+        string formattedResult = MailHelper.GenerateMailBody(results);
+        
+        Mailer.SendEmail(formattedResult);
+        
+        Console.WriteLine($"[{DateTime.Now}] Email sent successfully!");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"[{DateTime.Now}] Error: {ex.Message}");
+    }
+}
